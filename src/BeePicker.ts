dac5226c5fef66f456ad4c099fc62700afc754fe
Johan Lindfors <johan.lindfors@coderox.se>
2017-06-22 14:05:43 +0200

    Whitespace, not tabs

--- src/BeePicker.ts
+++ src/BeePicker.ts
@@ -1,85 +1,79 @@
 class BeePicker {
-	private bees: Array<Bee> = undefined;
-	private beeGroup: Phaser.Group;
+    private bees: Array<Bee> = undefined;
+    private beeGroup: Phaser.Group;
 
-	public constructor(game: Phaser.Game) {
-		this.bees = new Array<Bee>();
-		this.beeGroup = new Phaser.Group(game);
-		
-		for (var i = 0; i < 5; i++)
-		{
-			this.bees.push(new Bee(Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1))));
-			this.beeGroup.create(BEE_START_X + i * BEE_DELTA_X, BEE_START_Y,TEXTURE_BEE_MAP, 0);
-		}
-	}
+    public constructor(game: Phaser.Game) {
+        this.bees = new Array<Bee>();
+        this.beeGroup = new Phaser.Group(game);
 
-	public reset() {
-		this.bees.splice(0,this.bees.length);
-		
-		for (var i = 0; i < 5; i++)
-		{
-			this.bees.push(new Bee(Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1))));
-		}		
-	}
+        for (var i = 0; i < 5; i++) {
+            this.bees.push(new Bee(Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1))));
+            this.beeGroup.create(BEE_START_X + i * BEE_DELTA_X, BEE_START_Y,TEXTURE_BEE_MAP, 0);
+        }
+    }
 
-	public draw() {
-		for (var i = 0; i < 5; i++)
-		{
-			var sprite = <Phaser.Sprite>this.beeGroup.children[i];
-			sprite.frame = this.bees[i].color;
-			sprite.alpha = this.bees[i].isSelected ? 0.5 : 1.0;
-		}
-	}
+    public reset() {
+        this.bees.splice(0,this.bees.length);
 
-	public markSelectedBee(x: number) {
-		let index = Math.floor(x / BEE_DELTA_X);
-		this.bees[index].isSelected = true;
-	}
+        for (var i = 0; i < 5; i++) {
+            this.bees.push(new Bee(Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1))));
+        }
+    }
 
-	public getSelectedBee(x: number) : Bee {
-		let index = Math.floor(x / BEE_DELTA_X);
-		return this.bees[index];
-	}
+    public draw() {
+        for (var i = 0; i < 5; i++) {
+            var sprite = <Phaser.Sprite>this.beeGroup.children[i];
+            sprite.frame = this.bees[i].color;
+            sprite.alpha = this.bees[i].isSelected ? 0.5 : 1.0;
+        }
+    }
 
-	public removeAndReplaceBee(selectedBee: Bee, availableFlowers: Array<number>) {
-		let beeIndex = this.bees.indexOf(selectedBee);
+    public markSelectedBee(x: number) {
+        let index = Math.floor(x / BEE_DELTA_X);
+        this.bees[index].isSelected = true;
+    }
 
-		//check if we already have a bee that matches the available flowers 
-		//remember to skip over the selectedBee since it will be removed in a moment
-		let match = false;
-		let rainbowColor = 6;
+    public getSelectedBee(x: number) : Bee {
+        let index = Math.floor(x / BEE_DELTA_X);
+        return this.bees[index];
+    }
 
-		//if there is a rainbow flower it will always match
-		if (rainbowColor in availableFlowers)
-			match = true;
-		else
-		{
-			for (let i = 0; i < this.bees.length; i++)
-			{
-				if(i != beeIndex && this.bees[i].color in availableFlowers){
-					match = true;
-					break;
-				}
-			}
-		}
+    public removeAndReplaceBee(selectedBee: Bee, availableFlowers: Array<number>) {
+        let beeIndex = this.bees.indexOf(selectedBee);
 
-		var color;
-		if(match){
-			//we already have a match, just add a random colored bee
-			color = Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1));
-		}
-		else{
-			//we have no match so we must pick a color from the available colors
-			color = availableFlowers[Math.floor(Math.random() * (availableFlowers.length + 1))];
-		}
+        //check if we already have a bee that matches the available flowers 
+        //remember to skip over the selectedBee since it will be removed in a moment
+        let match = false;
+        let rainbowColor = 6;
 
-		//set the selected bee to the new color to "create" a new bee
-		this.bees[beeIndex].color = color;
-	}
+        //if there is a rainbow flower it will always match
+        if (rainbowColor in availableFlowers)
+            match = true;
+        else {
+            for (let i = 0; i < this.bees.length; i++) {
+                if(i != beeIndex && this.bees[i].color in availableFlowers) {
+                    match = true;
+                    break;
+                }
+            }
+        }
 
-	public deselectAll() {
-		this.bees.forEach(element => {
-			element.isSelected = false;
-		});
-	}
+        var color;
+        if(match) {
+            //we already have a match, just add a random colored bee
+            color = Math.floor(Math.random() * (NUMBER_OF_BEE_COLORS + 1));
+        } else {
+            //we have no match so we must pick a color from the available colors
+            color = availableFlowers[Math.floor(Math.random() * (availableFlowers.length + 1))];
+        }
+
+        //set the selected bee to the new color to "create" a new bee
+        this.bees[beeIndex].color = color;
+    }
+
+    public deselectAll() {
+        this.bees.forEach(element => {
+            element.isSelected = false;
+        });
+    }
 }
