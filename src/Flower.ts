f3bf41cc8532f6d0a43059448e2cc1089206b6a9
Johan Lindfors <johan.lindfors@coderox.se>
2017-07-03 20:48:42 +0200

    Clean up

--- src/Flower.ts
+++ src/Flower.ts
@@ -1,7 +1,5 @@
 class Flower extends Phaser.Sprite {
     color: number;
-    x: number;
-    y: number;
 
     constructor(game: Phaser.Game, x: number, y: number, key: string, frame: number) {
         super(game,x,y,key,frame);
