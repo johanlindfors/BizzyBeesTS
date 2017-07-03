9bcd7865df5acf3a6a333ea638938aa5985108e3
Johan Lindfors <johan.lindfors@coderox.se>
2017-07-03 21:19:24 +0200

    More cleanup

--- src/Column.ts
+++ src/Column.ts
@@ -1,20 +1,18 @@
 class Column extends Phaser.Group {
-    private col: number;
-
     velocity : number = INITAL_VELOCITY;
 
     public constructor(game: Phaser.Game, col: number) {
         super(game);
         game.add.existing(this);
 
-        this.col = col * FLOWER_COLUMN_WIDTH + FLOWER_COLUMN_MARGIN;
+        this.x = col * FLOWER_COLUMN_WIDTH + FLOWER_COLUMN_MARGIN;
         this.classType = Flower;
 
         this.reset();
         
         var mask = game.add.graphics(0,0);
         mask.beginFill(0xFFFFFF);
-        mask.drawRect(this.col, 170,100,650);
+        mask.drawRect(this.x,170,100,650);
         this.mask = mask;
     }
 
@@ -22,9 +20,9 @@ class Column extends Phaser.Group {
         this.removeAll();
 
         //add 3 flowers
-        this.addRandomFlower(this.col, COLUMN_TOP + 2 * FLOWER_DELTA_Y);
-        this.addRandomFlower(this.col, COLUMN_TOP + FLOWER_DELTA_Y);
-        this.addRandomFlower(this.col, COLUMN_TOP);
+        this.addRandomFlower(COLUMN_TOP + 2 * FLOWER_DELTA_Y);
+        this.addRandomFlower(COLUMN_TOP + FLOWER_DELTA_Y);
+        this.addRandomFlower(COLUMN_TOP);
 
         this.velocity = INITAL_VELOCITY;
         this.alive = true;
@@ -34,12 +32,12 @@ class Column extends Phaser.Group {
         return (this.length != 0 && this.children[0].y >= COLUMN_BOTTOM);
     }
 
-    private addRandomFlower(x: number, y: number) {
+    private addRandomFlower(y: number) {
         let color = Math.floor(Math.random() * (NUMBER_OF_FLOWER_COLORS + 1));
-        this.create(x,y,TEXTURE_FLOWER_MAP,color);
+        this.create(0,y,TEXTURE_FLOWER_MAP,color);
     }
 
-    public update() {
+    update() {
         if(this.alive){
 
             this.children.forEach(element => {
@@ -47,18 +45,18 @@ class Column extends Phaser.Group {
             });
 
             if (this.children.length == 0 || this.children[this.length - 1].y > COLUMN_TOP)
-                this.addRandomFlower(this.col, COLUMN_TOP - FLOWER_DELTA_Y);
+                this.addRandomFlower(COLUMN_TOP - FLOWER_DELTA_Y);
         }
     }
 
     public getBottomFlower() : any {
         if (this.length > 0)
-            return this.children[0];
+            return this.getChildAt(0);
         else
             return null;
     }
 
-    removeBottomFlower() {
+    public removeBottomFlower() {
         if (this.length > 0){
             this.removeChildAt(0);
         }
